409204cf9cb6d2dadfb389baf032caf9c7eb027c
Sam Radford <sam.radford@capitalroad.com.au>
2020-07-22 21:19:08 +1000

    block deletey

--- src/main/java/me/reilley/factory/blocks/Quarry.java
+++ src/main/java/me/reilley/factory/blocks/Quarry.java
@@ -3,10 +3,13 @@ package me.reilley.factory.blocks;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.Material;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.world.BlockView;
 
-public class Quarry extends Block {
+public class Quarry extends Block implements BlockEntityProvider {
     public Quarry() {
         super(
                 FabricBlockSettings.of(Material.METAL)
@@ -15,5 +18,10 @@ public class Quarry extends Block {
                         .sounds(BlockSoundGroup.METAL)
                         .strength(5, 6)
         );
+    }
+
+    @Override
+    public BlockEntity createBlockEntity(BlockView blockView) {
+        return new QuarryEntity();
     }
 }
