da4c5c882aa7472579cf4714582d61bfba177cd2
Pareido1ia <ryan.lewis117@gmail.com>
2020-08-09 01:14:50 +1000

    battery level

--- src/main/java/me/reilley/factory/block/entity/BatteryEntity.java
+++ src/main/java/me/reilley/factory/block/entity/BatteryEntity.java
@@ -1,6 +1,7 @@
 package me.reilley.factory.block.entity;
 
 import io.github.cottonmc.cotton.gui.PropertyDelegateHolder;
+import me.reilley.factory.block.Battery;
 import me.reilley.factory.block.GeneratorBlock;
 import me.reilley.factory.block.QuarryBlock;
 import me.reilley.factory.energy.FactoryEnergy;
@@ -26,27 +27,21 @@ import net.minecraft.util.Tickable;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.Direction;
 
-public class BatteryEntity extends BlockEntity implements FactoryEnergy, FactoryInventory, NamedScreenHandlerFactory, PropertyDelegateHolder, Tickable {
+public class BatteryEntity extends BlockEntity implements FactoryEnergy, PropertyDelegateHolder, Tickable {
     private final PropertyDelegate propertyDelegate;
-    private DefaultedList<ItemStack> inventory;
-    private int viewerCount;
     private double energy = 0;
 
     public BatteryEntity() {
         super(FactoryBlockEntityType.BATTERY);
-        this.inventory = DefaultedList.ofSize(1, ItemStack.EMPTY);
         this.propertyDelegate = new PropertyDelegate() {
             @Override
             public int get(int index) {
                 switch (index) {
                     case 0:
                         return (int) energy;
-
                     case 1:
                         return (int) getEnergyCapacity();
-
                 }
-
                 return -1;
             }
 
@@ -56,10 +51,8 @@ public class BatteryEntity extends BlockEntity implements FactoryEnergy, Factory
                     case 0:
                         energy = value;
                         break;
-
                     case 1:
                         break;
-
                 }
             }
 
@@ -73,15 +66,12 @@ public class BatteryEntity extends BlockEntity implements FactoryEnergy, Factory
     @Override
     public void fromTag(BlockState state, CompoundTag tag) {
         super.fromTag(state, tag);
-        this.inventory = DefaultedList.ofSize(this.size(), ItemStack.EMPTY);
-        Inventories.fromTag(tag, this.inventory);
         this.energy = tag.getShort("Energy");
     }
 
     @Override
     public CompoundTag toTag(CompoundTag tag) {
         super.toTag(tag);
-        Inventories.toTag(tag, this.inventory);
         tag.putShort("Energy", (short) this.energy);
         return tag;
     }
@@ -98,12 +88,12 @@ public class BatteryEntity extends BlockEntity implements FactoryEnergy, Factory
 
     @Override
     public double getEnergyCapacity() {
-        return 1000;
+        return 100;
     }
 
     @Override
     public double getMaxEnergyInput() {
-        return 0;
+        return 1;
     }
 
     @Override
@@ -117,72 +107,10 @@ public class BatteryEntity extends BlockEntity implements FactoryEnergy, Factory
     }
 
     @Override
-    public Text getDisplayName() {
-        return new TranslatableText(getCachedState().getBlock().getTranslationKey());
-    }
-
-    @Override
-    public ScreenHandler createMenu(int syncId, PlayerInventory inventory, PlayerEntity player) {
-        return new GeneratorBlockGuiDescription(syncId, inventory, ScreenHandlerContext.create(this.world, this.pos));
-    }
-
-    @Override
-    public DefaultedList<ItemStack> getInventory() {
-        return inventory;
-    }
-
-    @Override
-    public boolean canInsert(int slot, ItemStack stack, Direction dir) {
-        return this.isValid(slot, stack);
-    }
-
-    @Override
-    public boolean canPlayerUse(PlayerEntity player) {
-        if (this.world.getBlockEntity(this.pos) != this) {
-            return false;
-        } else {
-            return player.squaredDistanceTo((double) this.pos.getX() + 0.5D, (double) this.pos.getY() + 0.5D,
-                    (double) this.pos.getZ() + 0.5D) <= 64.0D;
-        }
-    }
-
-    @Override
-    public void onOpen(PlayerEntity player) {
-        if (!player.isSpectator()) {
-            if (this.viewerCount < 0) {
-                this.viewerCount = 0;
-            }
-
-            ++this.viewerCount;
-            this.onInvOpenOrClose();
-        }
-    }
-
-    @Override
-    public void onClose(PlayerEntity player) {
-        if (!player.isSpectator()) {
-            --this.viewerCount;
-            this.onInvOpenOrClose();
-        }
-    }
-
-    @Override
-    public boolean isValid(int slot, ItemStack itemStack) {
-        return AbstractFurnaceBlockEntity.canUseAsFuel(itemStack);
-    }
-
-    private void onInvOpenOrClose() {
-        Block block = this.getCachedState().getBlock();
-        if (block instanceof QuarryBlock) {
-            this.world.addSyncedBlockEvent(this.pos, block, 1, this.viewerCount);
-            this.world.updateNeighborsAlways(this.pos, block);
-        }
-    }
-
-    @Override
     public void tick() {
         if (!this.world.isClient) {
             energyTick(this.world, this.pos);
+            Battery.setIndicator(this.getEnergyCapacity(), this.getEnergy(), this.world, this.pos);
         }
     }
 }
